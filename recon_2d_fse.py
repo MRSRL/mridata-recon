747b5e046bfb3312b0342c0ae9c79a3267e2573f
Joseph Y Cheng <jycheng@stanford.edu>
2018-08-14 17:14:27 -0700

    Support NYU datasets, add more doc

--- recon_2d_fse.py
+++ recon_2d_fse.py
@@ -1,16 +1,28 @@
+"""Basic recon for 2D FSE datasets on mridata.org."""
 import numpy as np
 import os
-import glob
-import h5py
 import ismrmrd
-import xmltodict
 import argparse
+import imageio
 from tqdm import tqdm
 from fileio import cfl
 from mrirecon import fse
+from mrirecon import fftc
 
-def header_user_param_to_dict(header):
-    """Convert user parameters to dict."""
+def isrmrmd_user_param_to_dict(header):
+    """
+    Store ISMRMRD header user parameters in a dictionary.
+
+    Parameter
+    ---------
+    header : ismrmrd.xsd.ismrmrdHeader
+        ISMRMRD header object
+
+    Returns
+    -------
+    dict
+        Dictionary containing custom user parameters
+    """
     user_dict = {}
     user_long = list(header.userParameters.userParameterLong)
     user_double = list(header.userParameters.userParameterDouble)
@@ -22,11 +34,30 @@ def header_user_param_to_dict(header):
 
     return user_dict
 
-def dataset_to_np(file_name, verbose=False):
-    """Convert ISMRMRD to numpy array."""
+
+def load_ismrmrd_to_np(file_name, verbose=False):
+    """
+    Load data from an ISMRMRD file to a numpy array.
+
+    Raw data from the ISMRMRD file is loaded into a numpy array. If the ISMRMRD file includes the array 'rec_std' that contains the standard deviation of the noise, this information is used to pre-whiten the k-space data. If applicable, a basic phase correction is performed on the loaded k-space data.
+
+    Parameters
+    ----------
+    file_name : str
+        Name of ISMRMRD file
+    verbose : bool, optional
+        Turn on/off verbose print out
+
+    Returns
+    -------
+    np.array
+        k-space data in an np.array of dimensions [phase, echo, slice, coils, kz, ky, kx]
+    ismrmrd.xsd.ismrmrdHeader
+        ISMRMRD header object
+    """
     dataset = ismrmrd.Dataset(file_name, create_if_needed=False)
     header = ismrmrd.xsd.CreateFromDocument(dataset.read_xml_header())
-    param_dict = header_user_param_to_dict(header)
+    param_dict = isrmrmd_user_param_to_dict(header)
 
     num_kx = header.encoding[0].encodedSpace.matrixSize.x
     num_ky = header.encoding[0].encodingLimits.kspace_encoding_step_1.maximum + 1
@@ -121,12 +152,72 @@ def dataset_to_np(file_name, verbose=False):
             cfl.write("kspace_orig" , kspace)
         kspace = kspace_cor
 
-    return kspace
+    return kspace, header
 
 
-def dataset_to_cfl(dir_out, file_name, suffix="", verbose=False):
-    """Convert ISMRMRD to CFL."""
-    kspace = dataset_to_np(file_name, verbose=verbose)
+def transform(kspace, header, verbose=False):
+    """
+    Transform kspace data to image domain.
+
+    If needed, cropping is performed based on information in the header.
+
+    Parameters
+    ----------
+    kspace : np.array
+        Data in k-space [phase, echo, slice, coils, z, ky, kx]
+    header : ismrmrd.xsd.ismrmrdHeader
+        ISMRMRD header object
+    verbose : bool, optional
+        Turn on/off print outs
+
+    Returns
+    -------
+    np.array
+        Data in image domain [phase, echo, slice, coils, z, y, x]
+    """
+    image = fftc.ifft2c(kspace)
+
+    num_kx = header.encoding[0].encodedSpace.matrixSize.x
+    num_ky = header.encoding[0].encodingLimits.kspace_encoding_step_1.maximum + 1
+    num_x = header.encoding[0].reconSpace.matrixSize.x
+    num_y = header.encoding[0].reconSpace.matrixSize.y
+    if num_x < num_kx:
+        if verbose:
+            print("Cropping data in x (%d to %d)..." % (num_kx, num_x))
+        x0 = (num_kx - num_x) // 2
+        x1 = x0 + num_x
+        image = image[:, :, :, :, :, :, x0:x1]
+    if num_y < num_ky:
+        if verbose:
+            print("Cropping data in y (%d to %d)..." % (num_ky, num_y))
+        y0 = (num_ky - num_y) // 2
+        y1 = y0 + num_y
+        image = image[:, :, :, :, :, y0:y1, :]
+
+    return image
+
+
+def dataset_to_cfl(dir_out, file_name, suffix="", file_png=None, verbose=False):
+    """
+    Convert ISMRMRD to CFL files in specified directory.
+
+    Parameters
+    ----------
+    dir_out : str
+        Output directory to write CFL files
+    file_name : str
+        Name of ISMRMRD file
+    suffix : str, optional
+        Suffix to attach to output file names
+    file_png : str, optional
+        If not None, a png file will be written out
+    verbose : bool, optional
+        Turn on/off verbose print outs
+    """
+    kspace, header = load_ismrmrd_to_np(file_name, verbose=verbose)
+    if verbose:
+        print("Transforming k-space data to image domain...")
+    image = transform(kspace, header, verbose=verbose)
     num_phases = kspace.shape[0]
     num_echoes = kspace.shape[1]
 
@@ -142,6 +233,19 @@ def dataset_to_cfl(dir_out, file_name, suffix="", verbose=False):
 
             cfl.write(os.path.join(dir_out, "kspace" + suffix_i),
                       kspace[i_phase, i_echo, :, :, :, :, :])
+            cfl.write(os.path.join(dir_out, "image" + suffix_i),
+                      image[i_phase, i_echo, :, :, :, :, :])
+
+    if file_png is not None:
+        if verbose:
+            print("Writing example png...")
+        energy = np.sum(np.abs(kspace) ** 2, axis=(-1, -2, -4))
+        i_phase, i_echo, i_slice, i_z = np.where(energy == energy.max())
+        image_out = image[i_phase[0], i_echo[0], i_slice[0], :, i_z[0], :, :]
+        image_out = np.sqrt(np.sum(np.abs(image_out) ** 2, axis=0))
+        image_out = image_out / np.max(image_out) * np.iinfo(np.uint8).max
+        imageio.imwrite(file_png, image_out.astype(np.uint8))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process ISMRMRD files")
@@ -149,10 +253,13 @@ if __name__ == "__main__":
                         help="raw data in ISMRMRD format")
     parser.add_argument("-o", "--output", default="./",
                         help="output directory (default: ./)")
+    parser.add_argument("-p", "--png", default=None,
+                        help="write png file (default: None)")
     parser.add_argument("-s", "--suffix", default="",
                         help="suffix to file output")
     parser.add_argument("-v", "--verbose", action="store_true",
                         help="verbose printing (default: False)")
     args = parser.parse_args()
 
-    dataset_to_cfl(args.output, args.input, suffix=args.suffix, verbose=args.verbose)
+    dataset_to_cfl(args.output, args.input, suffix=args.suffix,
+                   file_png=args.png, verbose=args.verbose)
